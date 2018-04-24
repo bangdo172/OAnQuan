5b31cef6d6e8e1dc282e631f22618e6a1d40be2b
bangdo172 <17021211@vnu.edu.vn>
2018-04-25 02:47:41 +0700

    2018 04 25

--- game.cpp
+++ game.cpp
@@ -4,8 +4,6 @@
 
 #include "game.hpp"
 
-
-
 bool gEndGame;
 bool gTurnFirstPerson;
 int gFirstPlayerScore = 0;
@@ -49,22 +47,22 @@ void gameLoop() {
         }
         
         
-        int* a, *b;
-        
-        while( SDL_PollEvent( &e ) != 0 )
-        {
-            //User requests quit
-            if( e.type == SDL_QUIT )
-            {
-                quit = true;
-            }
-            
-            //Handle button events
-            for( int i = 0; i < TOTAL_BUTTONS; ++i )
-            {
-                gButtons[ i ].handleEvent( &e, a, b );
-            }
-        }
+//        int* a, *b;
+//
+//        while( SDL_PollEvent( &e ) != 0 )
+//        {
+//            //User requests quit
+//            if( e.type == SDL_QUIT )
+//            {
+//                quit = true;
+//            }
+//
+//            //Handle button events
+//            for( int i = 0; i < TOTAL_BUTTONS; ++i )
+//            {
+//                gButtons[ i ].handleEvent( &e, a, b );
+//            }
+//        }
         
         //Clear screen
         SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
